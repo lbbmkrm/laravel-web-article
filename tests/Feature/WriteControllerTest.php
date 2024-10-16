6eea464feb23a4b93d3de4030eb323145d91fb78
Labib Makarim <labibmakarim197@gmail.com>
2024-10-16 11:05:43 +0700

    Store

---
+++ tests/Feature/WriteControllerTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Category;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Log;
+use Tests\TestCase;
+
+class WriteControllerTest extends TestCase
+{
+    public function testStore() {}
+}
