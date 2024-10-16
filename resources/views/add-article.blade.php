4d5b28cc4520e5e5d9859d0936594c8f04643f91
Labib Makarim <labibmakarim197@gmail.com>
2024-10-16 11:08:32 +0700

    fix navbar

--- resources/views/add-article.blade.php
+++ resources/views/add-article.blade.php
@@ -7,7 +7,7 @@
   <nav class="bg-white border-gray-200 dark:bg-gray-900">
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
       <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
-        <img src="{{ asset('img/letter-a.png') }}" class="h-8" alt="Flowbite Logo" />
+        <img src="{{ asset('img/letter-a.png') }}" class="h-8" alt="Logo" />
         <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Articles</span>
       </a>
       <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -5,16 +5,23 @@
 <x-navbar></x-navbar>
 <main class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8 ">
 
-
-
-<article class="py-8 px-4 m-4 ">
-    <h2 class="mb-6 lg:text-6xl text-3xl text-center tracking-tight font-bold text-gray-900 "> {{ $post['tittle'] }}</h2>
-    <div class="mb-2 lg:text-2xl text-base text-gray-500">
-      <a class="text-blue-600 hover:text-blue-200 hover:underline" href="/author/{{  $post->user->username  }}">{{ $post->user->name }}</a> | <a href="/category/{{ $post->category->name }}" class=" hover:text-blue-200 hover:underline">{{ $post->category->name }}</a> | {{ $post->created_at->diffForHumans() }}
-    </div>
-  <p class="my-4 lg:font-2xl text-base font-light">{{ $post['body'] }}</p>
-  <a href="/" class="text-blue-600 font-medium hover:underline">&laquo;Back</a>
-</article>  
+<main class="pt-8 pb-16 lg:pt-16 lg:pb-24 bg-white dark:bg-gray-900 antialiased">
+  <div class="flex justify-between px-4 mx-auto max-w-screen-xl ">
+      <article class="mx-auto w-full max-w-2xl format format-sm sm:format-base lg:format-lg format-blue dark:format-invert">
+          <header class="mb-4 lg:mb-6 not-format">
+              <address class="flex items-center mb-6 not-italic">
+                  <div class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white">
+                      <img class="mr-4 w-16 h-16 rounded-full" src="{{ asset("img/user.png") }}" alt="Author">
+                      <div>
+                          <a href="/author/{{ $post->user->username}}" rel="author" class="text-xl font-bold text-gray-900 dark:text-white">{{ $post->user->name }}</a>
+                          <p class="text-base text-gray-500 dark:text-gray-400">{{ $post->created_at }}</p>
+                      </div>
+                  </div>
+              </address>
+              <h1 class="mb-4 text-3xl font-extrabold leading-tight text-gray-900 lg:mb-6 lg:text-4xl dark:text-white">{{ $post->tittle }}</h1>
+          </header>
+          <p class="lead">{{ $post->body }}
+          </p>
 
 
 </main>
